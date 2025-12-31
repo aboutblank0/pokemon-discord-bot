2676b28f7731ca6e8ade994ac12e170fce5d773c
sheizo <tiagov_999@hotmail.com>
2025-12-31 00:37:12 +0000

    initial .net project + update .gitignore

---
+++ bot/pokemon_discord_bot/pokemon_discord_bot/Main.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemon_discord_bot
+{
+    public class Program
+    {
+
+        
+
+        public static async Task Main()
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            client.Log += Log;
+
+            String token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+
+}
